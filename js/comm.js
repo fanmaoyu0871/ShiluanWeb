1618ddbdf3fc447a83b6fb3abdd808776571e26c
fanmaoyu0871 <frank0871@163.com>
2017-05-31 19:32:56 +0800

    finish 报警管理

--- js/comm.js
+++ js/comm.js
@@ -26,7 +26,7 @@ $(function () {
     var pre_index = -1;
     img_array.map(function (item, index, arr) {
         var li = $("<li><a index=" + index + " href='#'>" + title_array[index] + "</a></li>");
-        var url = "url('image/" + img_array[index] + "') no-repeat center 20px";
+        var url = "url('../image/" + img_array[index] + "') no-repeat center 20px";
         li.css({'background': url});
         ul.append(li);
     });
@@ -37,7 +37,7 @@ $(function () {
             return;
         } else {
             if (pre_index != -1) {
-                var pre_url = "url('image/" + img_array[pre_index] + "') no-repeat center 20px";
+                var pre_url = "url('../image/" + img_array[pre_index] + "') no-repeat center 20px";
                 $(".sl-left-box ul li a").eq(pre_index).css({
                     'background': pre_url,
                     'color': 'white',
@@ -49,8 +49,8 @@ $(function () {
             pre_index = index;
         }
 
-        var url = "url('image/" + img_select_array[index] + "') no-repeat center 20px";
-        var triangle_url = ", url('image/triangle.png') no-repeat right -2px center";
+        var url = "url('../image/" + img_select_array[index] + "') no-repeat center 20px";
+        var triangle_url = ", url('../image/triangle.png') no-repeat right -2px center";
         $(this).css({'background': url+triangle_url, 'color': '#8DD7F1', 'font-weight': '400'});
         $(this).parent('li').css({'background-color': 'black'});
     });
